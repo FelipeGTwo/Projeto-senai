9c457625d77ab6c30a83312ef85367c582fb14cc
FelipeGTwo <fgts38049@gmail.com>
2024-10-14 19:31:13 -0300

    atividades

--- app/app.js
+++ app/app.js
@@ -5,39 +5,55 @@ const path = require('path');
 const db = require('../database')
 
 
-app.get('/api-tester', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+ app.get('/api-tester', (req, res) => {
+     res.sendFile(path.join(__dirname, 'public/index.html'))
+ })
   
-app.get('/', (req, res) => {
-    res.send('Funcionou essa p****!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Funcionou essa p****!')
+// })
   
 
 app.use(express.json()); // Para permitir que o Express leia JSON no corpo das requisições
 
-app.post('/rota', (req, res) => {
+app.post('/tasks', (req, res) => {
     const dados = req.body
+    body = req.body.id
     console.log(dados)
-    res.send(`Dados recebidos: ${JSON.stringify(dados)}`)
 })
+
 
 app.listen(port, () => {
     console.log(`Rodando ${port}`)
 })
 
-//app.get('/usuario/:id', (req, res, next) => {
- //   db.all()
-  //  res.send(req.params.id)
- // })
-
-  app.get('/usuario/:id', (req, res) => {
-	parametro = req.params.id
-  db.get("SELECT * FROM user WHERE id = ?", parametro, (error, row) => {
-  	if(error) {
+app.get('/tasks', (req, res, next) => {
+   db.query("SELECT * FROM tasks", (error, row) => {
+   	if(error) {
     	res.json(error)
-      return
     }
     res.send(row)
-  })
-})
+   })
+ })
+
+// app.post('/tasks', (req, res, next) => {
+//  db.query("SELECT * FROM user WHERE id = ?", (error, row) => {
+//    if(error) {
+//     res.json(error)
+//   }
+//   res.send(row)
+//  })
+// })
+ 
+
+//   app.get('tasks', (req, res) => {
+// 	parametro = req.params.id
+//   db.get("SELECT * FROM user WHERE id = ?", parametro, (error, row) => {
+//   	if(error) {
+//     	res.json(error)
+//       return
+//     }
+//     res.send(row)
+//   })
+// })
+
--- database.js
+++ database.js
@@ -1,24 +1,51 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database("db.sqlite",   () => {
-    console.log('Banco de Dados conectado')
-    db.run(`CREATE TABLE user (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name VARCHAR(50),
-        email VARCHAR(50),
-        password VARCHAR(50)
-     )` , (error) => {
-        if (error) {
-            console.error('Deu ruim')
-            return;
-        }
-        var insertQuery = "INSERT INTO user (name, email, password) VALUES (?,?,?)"
-        db.run(insertQuery, ["usuario1", "usuario1@email.com", "123456"])
-        db.run(insertQuery, ["admin", "admin@email.com", "123456"])
-        }
-    )
+// const sqlite3 = require('sqlite3').verbose()
+// const db = new sqlite3.Database("db.sqlite",   () => {
+//     console.log('Banco de Dados conectado')
+//     db.run(`CREATE TABLE user (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         name VARCHAR(50),
+//         email VARCHAR(50),
+//         password VARCHAR(50)
+//      )` , (error) => {
+//         if (error) {
+//             console.error('Deu ruim')
+//             return;
+//         }
+//         var insertQuery = "INSERT INTO user (name, email, password) VALUES (?,?,?)"
+//         db.run(insertQuery, ["usuario1", "usuario1@email.com", "123456"])
+//         db.run(insertQuery, ["admin", "admin@email.com", "123456"])
+//         }
+//     )
 
-  console.log('Rodou os SQLs')
+//   console.log('Rodou os SQLs')
      
+// })
+
+// module.exports = db
+
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+  host: '',
+  user: '',
+  password: '',
+  database: 'senai-dev',
+  waitForConnections: true,   // Espera por conexões disponíveis
+  connectionLimit: 10,        // Limite máximo de conexões no pool
+  queueLimit: 0               // Número máximo de conexões em espera (0 = ilimitado)
 })
 
-module.exports = db
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack)
+    return
+  }
+  console.log('Connected as id ' + connection.threadId)
+
+  // Libera a conexão de volta para o pool após o uso
+  connection.release()
+})
+
+module.exports = pool
+
+
